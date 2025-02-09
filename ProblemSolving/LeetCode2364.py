531d462ba664c621342b1a4718ac1232b77c4b49
LouisVie61 <tantrieucao@gmail.com>
2025-02-09 12:42:15 +0700

    fix: counting factorial good pairs instead

--- ProblemSolving/LeetCode2364.py
+++ ProblemSolving/LeetCode2364.py
@@ -1,4 +1,6 @@
+import collections
 from typing import List
+import math
 
 
 def cntBadPairs(nums: List[int]) -> int:
@@ -10,5 +12,24 @@ def cntBadPairs(nums: List[int]) -> int:
                 cnt += 1
     return cnt
 
-arr = [1,2,3,4,5]
+def cntBadPairsWay2(nums: List[int]) -> int:
+    n = len(nums)
+    total_pairs = math.factorial(n) // (math.factorial(n - 2) * 2)
+    my_map = {}
+    for i in range(n):
+        my_map[i] = nums[i] - i
+
+    freq = [0] * (max(nums) + 1)
+    for v in my_map.values():
+        freq[v] += 1
+
+    cntGoodPairs = 0
+    for i in range(n):
+        if freq[i] > 1:
+            cntGoodPairs += (math.factorial(freq[i]) // (math.factorial(freq[i] - 2) * 2))
+
+    return total_pairs - cntGoodPairs
+
+arr = [4,1,3,3]
 print(cntBadPairs(arr))
+print(f'cach 2: {cntBadPairsWay2(arr)}')
