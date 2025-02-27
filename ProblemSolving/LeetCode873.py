5f318e88d55c01d8be672b2d4ad676c8eca4c4f7
LouisVie61 <tantrieucao@gmail.com>
2025-02-27 13:25:27 +0700

    feat: dynamic programming with new assignment

--- ProblemSolving/LeetCode873.py
+++ ProblemSolving/LeetCode873.py
@@ -3,12 +3,12 @@ from collections import defaultdict
 
 def lenLongestSubsequence(arr: list) -> int:
     index = {val: i for i, val in enumerate(arr)}
-    dp = defaultdict(lambda: 2) # fast lookup. create a default dictionary with the default length is two.
+    dp = defaultdict(lambda: 2) # create a default dictionary with the default length is two.
     max_length = 0
 
     for k in range(len(arr)):
         for j in range(k):
-            i = index.get(arr[k] - arr[j])
+            i = index.get(arr[k] - arr[j]) # fast lookup.
 
             if i is not None and i < j:
                 dp[(j, k)] = dp[(i, j)] + 1
